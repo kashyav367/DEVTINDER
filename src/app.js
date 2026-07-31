7f79a2b8bfa2c4d6fdc5c49b9bdec35b3c015df9
Ankit kumar Singh <ankit12030621028@gmail.com>
2026-07-31 22:58:06 +0530

    request-handle

--- src/app.js
+++ src/app.js
@@ -3,12 +3,45 @@ const express = require("express")
 const app = express()
 
 
-app.use("/test" , (req,res) => {
-    res.send("hello ji")
+
+// app.use("/hello/2" , (req,res) => {
+//     res.send("hello bachooo!!")
+// })
+
+// app.use("/hello" ,  (req,res) => {
+//     res.send("namaste ji")
+// })
+
+
+
+// app.use("/" , (req,res) => {
+//     res.send("kaise ho")
+// })
+
+
+app.get("/users" ,  (req,res) => {
+    res.send({firstname :"ankit", lastname: "singh"})
 })
 
-app.use("/hello" ,  (req,res) => {
-    res.send("namaste ji")
+app.post("/users" ,  (req,res) => {
+    res.send("save this data into database")
+})
+
+app.delete("/users" ,  (req,res) => {
+    res.send("deleted successfully")
+})
+
+
+app.put("/users", (req,res) => {
+    res.send("updated whole data")
+})
+
+app.patch("/users" , (req,res) => {
+    res.send("update specific data")
+})
+
+app.use("/test" , (req,res) => {
+    res.send("hello ji")
 })
 
 app.listen(7777, () => {
